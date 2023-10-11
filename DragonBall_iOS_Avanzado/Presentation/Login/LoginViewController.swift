20378e448c98111c8f5c2222ea64887b362f7268
marcomadv <imarcoma@gmail.com>
2023-10-11 12:09:28 +0200

    primer commit, añadimos storyboard de login + activity indicator

---
+++ DragonBall_iOS_Avanzado/Presentation/Login/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  DragonBall_iOS_Avanzado
+//
+//  Created by Marco Muñoz on 11/10/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailFieldError: UILabel!
+    @IBOutlet weak var passworldFieldError: UILabel!
+    @IBOutlet weak var loadingview: UIView!
+    
+    @IBAction func onLoginPressed() {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ DragonBall_iOS_AvanzadoUITests/DragonBall_iOS_AvanzadoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DragonBall_iOS_AvanzadoUITestsLaunchTests.swift
+//  DragonBall_iOS_AvanzadoUITests
+//
+//  Created by Marco Muñoz on 11/10/23.
+//
+
+import XCTest
+
+final class DragonBall_iOS_AvanzadoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
